0a1025a2bc74f22c238b3d4c47cdecafaa943e05
Murray Connelly Cutforth <mcc4@stanford.edu>
2025-03-27 15:59:36 -0700

    Added script to set off many runs

---
+++ scripts/set_off_runs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+start=$1
+end=$2
+
+for i in $(seq $start $end)
+do
+    padded_i=$(printf "%04d" $i)
+    echo "Starting run $padded_i"
+    cd $padded_i || exit
+    ./run-htr-with-restarts.sh &
+    cd ..
+done
