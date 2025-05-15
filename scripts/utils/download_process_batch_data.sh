a349f8ba6d1ddf871aae26c5fc1fedc2133256c5
Murray Connelly Cutforth <mcc4@stanford.edu>
2025-05-15 11:39:48 -0700

    Added batch 14, and postprocessing automation

--- scripts/utils/download_process_batch_data.sh
+++ scripts/utils/download_process_batch_data.sh
@@ -5,8 +5,16 @@ set -e
 
 USER=cutforth1
 HOST=lassen.llnl.gov
-BATCH="0013"
+BATCH=$1
 RUN_BATCH_DIR="/p/gpfs1/cutforth1/PSAAP/runs_batch_${BATCH}/[0-9][0-9][0-9][0-9]"
+
+# Check that BATCH is 4-digit number
+if [[ $BATCH =~ ^[0-9]{4}$ ]]; then
+    echo "Valid 4-digit batch number"
+else
+    echo "Invalid batch number - must be exactly 4 digits"
+    exit 1
+fi
 
 rsync -av --include='*/' --include='*/solution/*.out' --include='*/solution/*.csv' --include='*/solution/console-*.txt' --exclude '*' --rsync-path="env -i rsync" ${USER}@${HOST}:${RUN_BATCH_DIR} /Users/murray/Downloads/runs_batch_${BATCH}
 
